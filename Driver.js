72af709056e57942394a3d8ef5e6022c1f203db2
ramanimath <ramanimath@gmail.com>
2017-07-04 19:27:30 -0700

    Delete Driver.js

--- Driver.js
+++
@@ -1,9 +0,0 @@
-var eventproc = require('./fileutils/eventproc');
-var Graph = require('./models/datamodels').Graph;
-
-var inputfiledir = 'log_input';
-var outputfiledir = 'log_output';
-
-
-var graph = new Graph();
-eventproc.processfile(graph, inputfiledir, outputfiledir);
